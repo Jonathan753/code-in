bbb0b7744bf20d03d94536f8436d56035801d1f4
Jonathan Erik <jonathan753_21@hotmail.com>
2024-03-03 13:20:18 -0300

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import styles from "../styles/footer.module.css"
+export default function Footer () {
+    return(
+        <>
+        <div className={styles.footer}>
+            <p>Projeto &copy; 2024</p>
+        </div>
+        </>
+    );
+}
---
+++ components/Navbar.js
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import Image from 'next/image';
+import styles from "../styles/page.module.css";
+
+
+export default function Navbar() {
+
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
+                <Link href="/">
+                    <Image
+                        src="/logo.png"
+                        alt="Logo"
+                        width={45}
+                        height={45}
+                        priority
+                    />
+                </Link>
+            </div>
+            <div className={styles.navButtons}>
+                <Link href="/client">
+                    <p className={styles.link}>Inicio</p>
+                </Link>
+                <Link href="/client">
+                    <p className={styles.link}>Clientes</p>
+                </Link>
+            </div>
+        </nav>
+    );
+}
